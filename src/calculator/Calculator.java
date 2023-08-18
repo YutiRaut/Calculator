5906d85ee2815ef34745ea4d5b63071d457a6f97
rarti <yutiraut2002@gmail.com>
2023-08-18 11:46:35 +0530

    Initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class Calculator {
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        } else {
+            throw new ArithmeticException("Error: Cannot divide by zero");
+        }
+    }
+}
+
